ffd4b4d77f3e316fb307ce4a0f6e5ab3e94e8070
Lab-Java-OOP <60700646+xuatmon01@users.noreply.github.com>
2021-01-24 21:05:28 +0700

    new update

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,28 +1,44 @@
 import "../assets/css/SignIn.css";
+import {Component} from "react";
 
-function SignIn(props) {
-    const test = () =>{
-        window.location.assign("/signup")
-    }
-  return (
-    <div className="container-form">
+class SignIn extends Component {
+ 
+  render() {
+    return (
+      <div className="container-form">
+        <div className="form-box">
+          <div className="con-img">
+            <img src="./img/icon.png" alt="icon to do App" />
+          </div>
+          <div className="social-icon">
+            <img src="./img/fb.png" alt="" />
+            <img src="./img/gp.png" alt="" />
+            <img src="./img/tw.png" alt="" />
+          </div>
 
-      <div className="con-row">
-        <label htmlFor="">username:</label>
-        <input type="text" />
+          <form action="" className="input-group">
+            <input
+              type="text"
+              className="input-field"
+              placeholder="User name"
+              required
+            />
+            <input
+              type="password"
+              className="input-field"
+              placeholder="Enter password"
+              required
+            />
+            <input type="checkbox" className="check-box" />
+            <span>Remember password</span>
+            <button type="submit" className="btn-login">
+              Log in
+            </button>
+          </form>
+        </div>
       </div>
-
-      <div className="con-row">
-        <label htmlFor="">password:</label>
-        <input type="password" />
-      </div>
-
-      <div className="cont-btn">
-        <button onClick={test}>Sign In</button>
-      </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export { SignIn };
