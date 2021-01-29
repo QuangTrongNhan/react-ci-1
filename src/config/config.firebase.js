78881ec26f18040ef99fe0230350539ff7b12104
Lab-Java-OOP <60700646+xuatmon01@users.noreply.github.com>
2021-01-29 19:40:26 +0700

    form master

---
+++ src/config/config.firebase.js
@@ -0,0 +1,8 @@
+export  const firebaseConfig = {
+    apiKey: "AIzaSyB_Bg0AlYhr_7eddbg6np_gE1_9sFLhWCw",
+    authDomain: "dbp-cijs-d06-a6225.firebaseapp.com",
+    projectId: "dbp-cijs-d06-a6225",
+    storageBucket: "dbp-cijs-d06-a6225.appspot.com",
+    messagingSenderId: "868908453122",
+    appId: "1:868908453122:web:d7bf763a6a1a96450b461b"
+  };
